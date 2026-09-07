71cb2274dde75b5a9c98f13cf5f46af46d00a054
shreya <yourgithubemail@gmail.com>
2026-09-07 12:20:27 +0530

    replace the name of Sepahijala to Sipahijala

--- reimport-msme.js
+++ reimport-msme.js
@@ -15,7 +15,7 @@ const DISTRICT_MAP = {
   'DHALAI':        'Dhalai',
   'GOMATI':        'Gomati',
   'KHOWAI':        'Khowai',
-  'Sepahijala':    'Sepahijala',
+  'Sipahijala':    'Sipahijala',
   'UNAKOTI':       'Unakoti',
 };
 
